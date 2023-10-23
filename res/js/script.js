970cdf2f87c4b89dcfe1cf2d6e75fdbcf0b4e97d
0JP0 <janp3@gmx.de>
2023-10-23 18:16:47 +0300

    automatic posts

--- res/js/script.js
+++ res/js/script.js
@@ -15,3 +15,51 @@ window.onclick = function(event) {
     }
   }
 } 
+
+let jsonUri = "res/json/posts.json"
+window.onload = function () {
+  fetch(jsonUri) 
+        .then((response) => response.json())
+        .then(json => {
+            let postlist = document.getElementById("posts")
+            for(p of json){
+                let post = document.createElement("div")
+                post.classList.toggle("post")
+                //header
+                let postheader = document.createElement("div")
+                post.classList.toggle("posthead")
+                let headerimage = document.createElement("img")
+                headerimage.src="res/images/me.png"
+                headerimage.alt="LA"
+                let headerdate = document.createElement("p")
+                headerdate.innerText=p.date
+                postheader.append(headerimage,headerdate)
+                //body
+                let postbody = document.createElement("div")
+                post.classList.toggle("postbody")
+                let bodyimage = document.createElement("img")
+                bodyimage.src=p.pictureURI
+                let bodytext = document.createElement("p")
+                bodytext.innerText=p.text
+                if(p.pictureURI!==""||p.pictureURI!=""){
+                  postbody.appendChild(bodyimage)
+                }
+                postbody.appendChild(bodytext)
+                //footer
+                let postfooter = document.createElement("div")
+                post.classList.toggle("postfooter")
+                let footerimage = document.createElement("img")
+                footerimage.src="res/images/likebutton.png"
+                postfooter.append(footerimage)
+
+                post.append(postheader,postbody,postfooter)
+                postlist.appendChild(post)
+            }
+        })
+        .catch(err => {
+            let errDiv = document.createElement("div");
+            errDiv.className = 'post';
+            errDiv.innerText = err;
+            document.body.appendChild(errDiv);
+            });
+}
